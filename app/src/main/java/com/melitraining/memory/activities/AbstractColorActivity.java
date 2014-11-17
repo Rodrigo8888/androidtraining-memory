9e1f48ef68b0cab8c49da3bb5324a87f40e53cb1
uname <uname@email.com>
2014-11-17 16:14:01 -0200

    Memory

--- app/src/main/java/com/melitraining/memory/activities/AbstractColorActivity.java
+++ app/src/main/java/com/melitraining/memory/activities/AbstractColorActivity.java
@@ -2,6 +2,7 @@ package com.melitraining.memory.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.GestureDetector;
@@ -9,10 +10,9 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import com.melitraining.memory.R;
-import com.melitraining.memory.activities.colors.CyanActivity;
 import com.melitraining.memory.activities.colors.BlueActivity;
+import com.melitraining.memory.activities.colors.CyanActivity;
 import com.melitraining.memory.activities.colors.GreenActivity;
 import com.melitraining.memory.activities.colors.RedActivity;
 import com.melitraining.memory.tracking.StepsTracker;
@@ -22,16 +22,18 @@ import com.melitraining.memory.util.GlobalCounter;
  * Created by ngiagnoni on 11/12/14.
  */
 public abstract class AbstractColorActivity extends Activity {
+    RelativeLayout container;
     @Override
     protected final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abstract_color);
-        RelativeLayout container = (RelativeLayout) findViewById(R.id.color);
+        container = (RelativeLayout) findViewById(R.id.color);
         TextView counter = (TextView) findViewById(R.id.counter);
         counter.setText(String.valueOf(GlobalCounter.COUNTER));
         container.setBackground(getDrawable());
         container.setOnTouchListener(listener);
-        StepsTracker.getInstance().addActivityStep(this);
+        StepsTracker.getInstance().addActivityStep(this.getLocalClassName());
+
     }
 
 
@@ -42,27 +44,31 @@ public abstract class AbstractColorActivity extends Activity {
         GlobalCounter.COUNTER++;
         Intent i = new Intent(this, clasz);
         startActivity(i);
+
     }
 
     private void onRightToLeftSwipe() {
         startColorActivity(GreenActivity.class);
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+
     }
 
     private void onLeftToRightSwipe() {
-
         startColorActivity(BlueActivity.class);
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+
     }
 
     public void onTopToBottomSwipe() {
         startColorActivity(RedActivity.class);
         overridePendingTransition(R.anim.slide_in_up, R.anim.slide_out_down);
+
     }
 
     public void onBottomToTopSwipe() {
         startColorActivity(CyanActivity.class);
         overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_up);
+
     }
 
     public void onLongTap() {
@@ -81,6 +87,8 @@ public abstract class AbstractColorActivity extends Activity {
         super.onBackPressed();
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
+
+
 
     private View.OnTouchListener listener = new View.OnTouchListener() {
         @Override
@@ -147,7 +155,14 @@ public abstract class AbstractColorActivity extends Activity {
         if (level == TRIM_MEMORY_UI_HIDDEN){
             StepsTracker.getInstance().trackCurrentSteps();
         }
-
         super.onTrimMemory(level);
+    }
+
+    @Override
+    protected void onStop() {
+        ((BitmapDrawable)container.getBackground()).getBitmap().recycle();
+        container.setBackground(null);
+        super.onStop();
+
     }
 }
--- app/src/main/java/com/melitraining/memory/activities/colors/RedActivity.java
+++ app/src/main/java/com/melitraining/memory/activities/colors/RedActivity.java
@@ -1,7 +1,10 @@
 package com.melitraining.memory.activities.colors;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-
 import com.melitraining.memory.R;
 import com.melitraining.memory.activities.AbstractColorActivity;
 
@@ -9,13 +12,66 @@ import com.melitraining.memory.activities.AbstractColorActivity;
  * Created by ngiagnoni on 11/12/14.
  */
 public class RedActivity extends AbstractColorActivity{
+
     @Override
     protected int getBackColor() {
         return getResources().getColor(R.color.red);
     }
-
+    Bitmap fondo;
     @Override
     protected Drawable getDrawable() {
-        return getResources().getDrawable(R.drawable.red);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), R.drawable.red, options);
+        int imageHeight = options.outHeight;
+        int imageWidth = options.outWidth;
+        String imageType = options.outMimeType;
+         fondo = decodeSampledBitmapFromResource(getResources(), R.drawable.red, 100, 100);
+        Drawable d = new BitmapDrawable(getResources(),fondo);
+        return d;
+    }
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 6;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        fondo.recycle();
     }
 }
